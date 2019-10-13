1e067e9ef0b81c55ef6040c5075be4a0fb4de98e
Samer218 <48079116+Samer218@users.noreply.github.com>
2019-10-14 09:53:32 +1100

    Update Code.sql
    Task 2 Completed

--- Code.sql
+++ Code.sql
@@ -20,3 +20,41 @@ Primary Key(ClientID)
 -- Task 1 Completed
 
 --Task 2
+DROP TABLE IF EXISTS Tour;
+DROP TABLE IF EXISTS Events;
+DROP TABLE IF EXISTS Client;
+DROP TABLE IF EXISTS Booking;
+
+Create Table Tour (
+TourName NVARCHAR(100)  
+, Descriptions NVARCHAR(500)
+Primary Key (TourName)
+);
+
+Create Table Events (
+TYMDID NVARCHAR(200) 
+, EventYear INT 
+, EventMonth NVARCHAR(3)
+, EventDay INT
+, Fee MONEY
+, TourName NVARCHAR(100)
+Primary Key (TYMDID)
+Foreign Key (TourName) references Tour
+);
+
+Create Table Client (
+ClientID INT Primary Key
+, Surname NVARCHAR(100) 
+, GivenName NVARCHAR(100) 
+, Gender NVARCHAR(1) 
+);
+
+Create Table Booking (
+CYMDID NVARCHAR(300) Primary Key
+, DateBooked DATE
+, Payment MONEY
+, TYMDID NVARCHAR(200) Foreign Key (TYMDID) references Events
+, ClientID INT Foreign Key (ClientID) references Client
+);
+
+--Task 2 Completed
